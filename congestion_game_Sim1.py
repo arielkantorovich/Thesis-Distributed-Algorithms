33bf805ec041b1079b5c38087c7f44076025bf22
Ariel Kantorovich <ubuntu@ARIELKA-LAP.EMIT.local>
2023-12-01 10:25:19 +0200

    Congestion with global Grad (Bug)

--- congestion_game_Sim1.py
+++ congestion_game_Sim1.py
@@ -7,7 +7,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
-def Draw_Graph(G):
+def Draw_Graph(G, draw_edges=False):
     """
     The function drawing unDirected graph with edges
     :param G:
@@ -16,12 +16,14 @@ def Draw_Graph(G):
     node_colors = ["red" if node == "S" else "green" if node == "T" else "skyblue" for node in G.nodes]
     pos = nx.spring_layout(G)
     nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700, node_color=node_colors)
-    # edge_labels = nx.get_edge_attributes(G, 'weight')
-    # for edge, weight in edge_labels.items():
-    #     x, y = pos[edge[0]]
-    #     x_, y_ = pos[edge[1]]
-    #     mid_x, mid_y = (x + x_) / 2, (y + y_) / 2
-    #     plt.text(mid_x, mid_y, s=f'{weight:.2f}', bbox=dict(facecolor='white', alpha=0.5), ha='center', va='center')
+
+    if draw_edges:
+        edge_labels = nx.get_edge_attributes(G, 'weight')
+        for edge, weight in edge_labels.items():
+            x, y = pos[edge[0]]
+            x_, y_ = pos[edge[1]]
+            mid_x, mid_y = (x + x_) / 2, (y + y_) / 2
+            plt.text(mid_x, mid_y, s=f'{weight:.2f}', bbox=dict(facecolor='white', alpha=0.5), ha='center', va='center')
     plt.show()
 
 def Generate_graph(num_nodes=60, p=0.1):
@@ -44,17 +46,14 @@ def find_all_paths(graph, source, target):
     """
     return list(nx.all_simple_paths(graph, source=source, target=target))
 
-def Generate_Game_Param(nodes):
+def Generate_Game_Param(K):
     """
-    :param nodes:
+    :param K:(int), Number of strategy in game
     :return:
     """
-    A = np.random.rand(nodes, nodes)
-    np.fill_diagonal(A, 0)
-    B = np.random.rand(nodes, nodes)
-    np.fill_diagonal(B, 0)
-    C = np.random.rand(nodes, nodes)
-    np.fill_diagonal(C, 0)
+    A = np.random.rand(K,)
+    B = np.random.rand(K,)
+    C = np.random.rand(K,)
     return A, B, C
 
 def Initialize_action(N, P):
@@ -64,37 +63,132 @@ def Initialize_action(N, P):
     :param P: (int) number of paths from source to targer
     :return: Xe (2D-array) size NxP
     """
-    Xe = np.random.rand(N, P)
-    row_sums = np.sum(Xe, axis=1, keepdims=True)
-    Xe /= row_sums
-    return Xe
+    Xn_k = np.random.rand(N, P)
+    row_sums = np.sum(Xn_k, axis=1, keepdims=True)
+    Xn_k /= row_sums
+    return Xn_k
+
+def calc_gradient(C_k, Xn_k, B, C, Xe, Nash_flag=False):
+    """
+    :param C_k: size (K, )
+    :param Xn_k: size (N, K)
+    :param B: size (K,)
+    :param C: size (K,)
+    :param Nash_flag: bool, specify if calculate only gradient Nash
+    :return: gradient of each player size (N, )
+    """
+    self_gradient = C_k + Xn_k * (B + 2 * C * Xe)
+    if Nash_flag:
+        return self_gradient
+    global_grad = C_k + (B + 2 * C * Xe) * np.sum(Xn_k, axis=0, keepdims=True)
+    global_grad = np.ones_like(Xn_k) * global_grad
+    return global_grad
+
+
+def project_onto_simplex(V, z=1):
+    """
+    Project matrix X onto the probability simplex.
+    :param Y: numpy array, matrix of size NxK where D=K
+    :param z: integer, norm of projected vector
+    :return X: numpy array, matrix of the same size as X, projected onto the probability simplex
+    """
+    n_features = V.shape[1]
+    U = np.sort(V, axis=1)[:, ::-1]
+    z = np.ones(len(V)) * z
+    cssv = np.cumsum(U, axis=1) - z[:, np.newaxis]
+    ind = np.arange(n_features) + 1
+    cond = U - cssv / ind > 0
+    rho = np.count_nonzero(cond, axis=1)
+    theta = cssv[np.arange(len(V)), rho - 1] / rho
+    return np.maximum(V - theta[:, np.newaxis], 0)
+
 
 # Hyper Parameters
 prob = 0.1
-nodes = 15
-N = 100 # Number of players
+nodes = 20
+N = 20 # Number of players
+T = 2000
+lr = 0.03 * np.reciprocal(np.power(range(1, T + 1), 0.75))
+K = 5 # Number of strategy
 
-# Generate Graph
-G = Generate_graph(nodes, prob)
+# Record Variables
+nash_Cn_t = np.zeros((T, N)) # record cost of players
+global_Cn_t = np.zeros((T, N)) # record cost of players
+global_grad_t = np.zeros((T, N, K)) # record gradient
+nash_grad_t = np.zeros((T, N, K)) # record gradient
+global_action_record = np.zeros((T, N, K)) # Record action of players
+nash_action_record = np.zeros((T, N, K))
+global_record = np.zeros((T, ))
+nash_record = np.zeros((T, ))
+#############################################################################
+# # Generate Graph
+# G = Generate_graph(nodes, prob)
+#
+# # Checking connected graph
+# while not(nx.is_connected(G)):
+#     G = Generate_graph(nodes, prob)
+#
+# # Draw Graph
+# Draw_Graph(G, draw_edges=False)
+#
+# # Find all paths from source (node 0) to target (node N-1)
+# paths = find_all_paths(G, source="S", target="T")
+# K = len(paths) # Number of strategy
 
-# Checking connected graph
-while not(nx.is_connected(G)):
-    G = Generate_graph(nodes, prob)
-
-# Draw Graph
-Draw_Graph(G)
-
-# Find all paths from source (node 0) to target (node N-1)
-paths = find_all_paths(G, source="S", target="T")
-
-# Extract Graph as adjacent Matrix
-adj_matrix = nx.to_numpy_array(G, dtype=float)
-
+# # Extract Graph as adjacent Matrix
+# adj_matrix = nx.to_numpy_array(G, dtype=float)
+#####################################################################################################
 # Generate A, B, C Parameters
-A, B, C = Generate_Game_Param(nodes)
+A, B, C = Generate_Game_Param(K)
 
 # Generate action of players
-Xe = Initialize_action(N, len(paths))
+nash_Xn_k = Initialize_action(N, K)
+global_Xn_k = nash_Xn_k.copy()
 
+
+# Main loop
+for t in range(T):
+    # Calculate load
+    Xe_nash = np.sum(nash_Xn_k, axis=0)
+    Xe_global = np.sum(global_Xn_k, axis=0)
+
+    # calculate congestion
+    C_k_nash = A + B * Xe_nash + C * (Xe_nash ** 2)
+    C_k_global = A + B * Xe_global + C * (Xe_global ** 2)
+
+    # calculate cost of player n
+    C_n_nash = np.sum(nash_Xn_k * C_k_nash, axis=1)
+    C_n_global = np.sum(global_Xn_k * C_k_global, axis=1)
+    nash_Cn_t[t] = C_n_nash
+    global_Cn_t[t] = C_n_global
+
+    # Calculate Global & Nash record
+    global_record[t] = np.sum(C_n_nash)
+    nash_record[t] = np.sum(C_n_global)
+
+    # calculate gradient
+    grad_nash = calc_gradient(C_k_nash, nash_Xn_k, B, C, Xe_nash, Nash_flag=True)
+    grad_global = calc_gradient(C_k_global, global_Xn_k, B, C, Xe_global, Nash_flag=False)
+    global_grad_t[t] = grad_global
+    nash_grad_t[t] = grad_nash
+
+    # Update action of players
+    nash_Xn_k = nash_Xn_k - lr[t] * grad_nash
+    global_Xn_k = global_Xn_k - lr[t] * grad_global
+    global_action_record[t] = global_Xn_k
+    nash_action_record[t] = nash_Xn_k
+
+    # Project gradient to constraint
+    nash_Xn_k = project_onto_simplex(nash_Xn_k)
+    global_Xn_k = project_onto_simplex(global_Xn_k)
+
+
+# Plot results
+t = np.arange(T)
+plt.figure(1)
+plt.plot(t, global_record, label="Global")
+plt.plot(t, nash_record, label="Nash")
+plt.xlabel("# Iterations"), plt.ylabel("# Score"), plt.legend()
+plt.show()
 
 print("Finsh")
