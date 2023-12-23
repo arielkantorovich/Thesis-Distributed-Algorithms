66d266d61c6aad23c8515232e15dd3dc73dac2d5
Ariel Kantorovich <ubuntu@ARIELKA-LAP.EMIT.local>
2023-12-23 14:13:33 +0200

    Implement 3 samples

--- congestion_game_Sim1.py
+++ congestion_game_Sim1.py
@@ -14,23 +14,19 @@ import torch.nn.functional as F
 
 
 class CongestionCNN(nn.Module):
-    def __init__(self, input_channels=1, output_size=4):
+    def __init__(self, input_channels=1, output_size=8):
         super(CongestionCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=(3, 3), padding=1)
-        self.bn_conv1 = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3))
-        self.bn_conv2 = nn.BatchNorm2d(128)
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=16, kernel_size=(3, 3), padding=1)
+        self.bn_conv1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=1)
+        self.bn_conv2 = nn.BatchNorm2d(32)
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(128 * 1 * 2, 128)
+        self.fc1 = nn.Linear(32 * 9 * 4, 128)
         self.bn1 = nn.BatchNorm1d(128)
-        self.fc2 = nn.Linear(128, 64)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.fc3 = nn.Linear(64, 32)
-        self.bn3 = nn.BatchNorm1d(32)
-        self.fc4 = nn.Linear(32, 16)
-        self.bn4 = nn.BatchNorm1d(16)
-        self.fc5 = nn.Linear(16, output_size)
+        self.fc2 = nn.Linear(128, 16)
+        self.bn2 = nn.BatchNorm1d(16)
+        self.fc3 = nn.Linear(16, output_size)
 
     def forward(self, x):
         x = self.relu(self.bn_conv1(self.conv1(x)))
@@ -39,38 +35,34 @@ class CongestionCNN(nn.Module):
         # Fully connected
         x = self.relu(self.bn1(self.fc1(x)))
         x = self.relu(self.bn2(self.fc2(x)))
-        x = self.relu(self.bn3(self.fc3(x)))
-        x = self.relu(self.bn4(self.fc4(x)))
-        x = self.relu(self.fc5(x))
+        x = self.relu(self.fc3(x))
+
         return x
-class Congestion_AutoEncoder(nn.Module):
+class Congestion(nn.Module):
     def __init__(self, input_size, output_size):
-        super(Congestion_AutoEncoder, self).__init__()
-        self.fc1 = nn.Linear(input_size, 32)
-        self.bn1 = nn.BatchNorm1d(32)
-        self.fc2 = nn.Linear(32, 64)
+        super(Congestion, self).__init__()
+        self.fc0 = nn.Linear(input_size, 256)
+        self.bn0 = nn.BatchNorm1d(256)
+        self.fc1 = nn.Linear(256, 128)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.fc2 = nn.Linear(128, 64)
         self.bn2 = nn.BatchNorm1d(64)
-        self.fc3 = nn.Linear(64, 128)
-        self.bn3 = nn.BatchNorm1d(128)
-        self.fc4 = nn.Linear(128, 64)
-        self.bn4 = nn.BatchNorm1d(64)
-        self.fc5 = nn.Linear(64, 32)
-        self.bn5 = nn.BatchNorm1d(32)
-        self.fc6 = nn.Linear(32, output_size)
+        self.fc3 = nn.Linear(64, 32)
+        self.bn3 = nn.BatchNorm1d(32)
+        self.fc4 = nn.Linear(32, 16)
+        self.bn4 = nn.BatchNorm1d(16)
+        self.fc5 = nn.Linear(16, output_size)
         self.init_weights()
 
     def forward(self, x):
-        # Encoder Part
-        x1 = torch.relu(self.bn1(self.fc1(x)))
-        x2 = torch.relu(self.bn2(self.fc2(x1)))
-        x3 = torch.relu(self.bn3(self.fc3(x2)))
-        # Decoder Part
-        x4 = torch.relu(self.bn4(self.fc4(x3))) + x2
-        x5 = torch.relu(self.bn5(self.fc5(x4))) + x1
 
-        # Final layer without activation for regression
-        output = self.fc6(x5)
-        return output
+        x = F.relu(self.bn0(self.fc0(x)))
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = F.relu(self.bn3(self.fc3(x)))
+        x = F.relu(self.bn4(self.fc4(x)))
+        x = F.relu(self.fc5(x))
+        return x
 
     def init_weights(self):
         for m in self.modules():
@@ -83,29 +75,21 @@ class Congestion_AutoEncoder(nn.Module):
                 init.constant_(m.weight, 1)
                 init.constant_(m.bias, 0)
 
-class Congestion(nn.Module):
+
+class Shallow_Congestion(nn.Module):
     def __init__(self, input_size, output_size):
-        super(Congestion, self).__init__()
-        self.fc0 = nn.Linear(input_size, 128)
-        self.bn0 = nn.BatchNorm1d(128)
-        self.fc1 = nn.Linear(128, 64)
+        super(Shallow_Congestion, self).__init__()
+        self.fc0 = nn.Linear(input_size, 256)
+        self.bn0 = nn.BatchNorm1d(256)
+        self.fc1 = nn.Linear(256, 64)
         self.bn1 = nn.BatchNorm1d(64)
-        self.fc2 = nn.Linear(64, 32)
-        self.bn2 = nn.BatchNorm1d(32)
-        self.fc3 = nn.Linear(32, 16)
-        self.bn3 = nn.BatchNorm1d(16)
-        self.fc4 = nn.Linear(16, 8)
-        self.bn4 = nn.BatchNorm1d(8)
-        self.fc5 = nn.Linear(8, output_size)
+        self.fc2 = nn.Linear(64, output_size)
         self.init_weights()
 
     def forward(self, x):
         x = F.relu(self.bn0(self.fc0(x)))
         x = F.relu(self.bn1(self.fc1(x)))
-        x = F.relu(self.bn2(self.fc2(x)))
-        x = F.relu(self.bn3(self.fc3(x)))
-        x = F.relu(self.bn4(self.fc4(x)))
-        x = F.relu(self.fc5(x))
+        x = F.relu(self.fc2(x))
         return x
 
     def init_weights(self):
@@ -275,9 +259,22 @@ def EST_ABS(Ck, Xe, sigma=2):
     C = np.repeat(C[:, np.newaxis, :], N, axis=1)
     return A, B, C
 
+def compute_beta(Xn_k, Xe, a1, a2, b1, b2):
+    """
+    :param Xn_k:
+    :param Xe:
+    :param a1:
+    :param a2:
+    :param b1:
+    :param b2:
+    :return:
+    """
+    beta = 2 * a1 * (Xe ** 2) + a2 * Xe + b2 * Xn_k + 2 * b1 * Xn_k * Xe
+    return beta
+
 
 # Define Seed for debug
-np.random.seed(0)
+# np.random.seed(0)
 
 # Hyper Parameters
 prob = 0.1
@@ -292,6 +289,7 @@ lr_nash_const = 0.03 * np.reciprocal(np.power(range(1, T + 1), 0.75))
 Debug_flag = False
 Generate_Train = False
 Normalize_flag = False
+Random_flag = False
 
 # Record Variables
 if Debug_flag:
@@ -328,7 +326,6 @@ nash_tax_record = np.zeros((T, L), dtype=np.float32)
 # Generate A, B, C Parameters
 A, B, C = Generate_Game_Param(L, K)
 
-
 # Generate action of players
 nash_Xn_k = Initialize_action(L, N, K)
 global_Xn_k = nash_Xn_k.copy()
@@ -337,24 +334,54 @@ nash_tax_Xn_k = nash_Xn_k.copy()
 
 # Prepare data to Network
 Xe_nash = np.sum(nash_Xn_k, axis=1)
-Xe_nash_expand = np.repeat(Xe_nash[:, np.newaxis, :], N, axis=1)
+# Xe_nash_expand = np.repeat(Xe_nash[:, np.newaxis, :], N, axis=1)
 C_k_nash = A + B * Xe_nash + C * (Xe_nash ** 2)
-C_k_nash_expanded = np.repeat(C_k_nash[:, np.newaxis, :], N, axis=1)
+# C_k_nash_expanded = np.repeat(C_k_nash[:, np.newaxis, :], N, axis=1)
 
 # Save Data for ML training
 if Generate_Train:
+    # C_expand = np.repeat(C[:, np.newaxis, :], N, axis=1)
+    # B_expand = np.repeat(B[:, np.newaxis, :], N, axis=1)
     # Save arrays
-    np.save("Numpy_array_save/Xn_k.npy", nash_Xn_k)
-    np.save("Numpy_array_save/Xe.npy", Xe_nash_expand)
-    np.save("Numpy_array_save/Ck.npy", C_k_nash_expanded)
+    # np.save("Numpy_array_save/Xn_k.npy", nash_Xn_k)
+    np.save("Numpy_array_save/Xe.npy", Xe_nash)
+    np.save("Numpy_array_save/Ck.npy", C_k_nash)
+    np.save("Numpy_array_save/B.npy", B)
+    np.save("Numpy_array_save/C.npy", C)
+    for i in range(2):
+        nash_Xn_k = Initialize_action(L, N, K)
+        Xe_nash = np.sum(nash_Xn_k, axis=1)
+        C_k_nash = A + B * Xe_nash + C * (Xe_nash ** 2)
+        np.save(f"Numpy_array_save/Xe_{i}.npy", Xe_nash)
+        np.save(f"Numpy_array_save/Ck_{i}.npy", C_k_nash)
+
 
 # Prepare Data to Network
-# A_G, B_G, C_G = EST_ABS(C_k_nash, Xe_nash, sigma=2)
-ONE = np.ones_like(Xe_nash_expand)
-X_test = np.concatenate([nash_Xn_k, C_k_nash_expanded, ONE, Xe_nash_expand, Xe_nash_expand ** 2], axis=-1)
-X_test = np.reshape(X_test, (L * N, 5 * K))
-# X_test = np.concatenate((nash_Xn_k, Xe_nash_expand, C_k_nash_expanded), axis=2)
-# X_test = X_test.reshape(N*L, 1, K, 3)
+Xn_k_0 = Initialize_action(L, N, K)
+Xe_0 = np.sum(Xn_k_0, axis=1)
+C_k_0 = A + B * Xe_0 + C * (Xe_0 ** 2)
+Xn_k_1 = Initialize_action(L, N, K)
+Xe_1 = np.sum(Xn_k_1, axis=1)
+C_k_1 = A + B * Xe_1 + C * (Xe_1 ** 2)
+
+# For CNN network
+# xe_reshaped = Xe_nash.reshape(L, K, 1)
+# ck_reshaped = C_k_nash.reshape(L, K, 1)
+# xe_sqaure_reshape = xe_reshaped ** 2
+# xe0_reshaped = Xe_0.reshape(L, K, 1)
+# ck0_reshaped = C_k_0.reshape(L, K, 1)
+# xe0_sqaure_reshape = xe0_reshaped ** 2
+# xe1_reshaped = Xe_1.reshape(L, K, 1)
+# ck1_reshaped = C_k_1.reshape(L, K, 1)
+# xe1_sqaure_reshape = xe1_reshaped ** 2
+# X_test = np.concatenate((ck_reshaped, xe_reshaped, xe_sqaure_reshape,
+#                                 ck0_reshaped, xe0_reshaped, xe0_sqaure_reshape,
+#                                 ck1_reshaped, xe1_reshaped, xe1_sqaure_reshape), axis=2)
+# X_test = X_test.reshape(L, 1, K, 9)
+
+X_test = np.concatenate([C_k_nash, Xe_nash, Xe_nash ** 2, C_k_0, Xe_0, Xe_0 ** 2,  C_k_1, Xe_1, Xe_1 ** 2], axis=-1)
+X_test = np.reshape(X_test, (L, 9 * K))
+
 
 if Normalize_flag:
     file_path_mean = os.path.join("Numpy_array_save", "X_mean(L=20k).npy")
@@ -366,9 +393,9 @@ if Normalize_flag:
 
 # Get Beta from Neural Network
 device = ["cuda" if torch.cuda.is_available() else "cpu"]
-file_path_weights = os.path.join("trains_record", "Congestion_game_with_Adam", "Congestion(Relu_SGD).pth")
-input_size = 5 * K
-output_size = K
+file_path_weights = os.path.join("trains_record", "Congestion_game_with_Adam", "Congestion.pth")
+input_size = 9 * K
+output_size = 2 * K
 model = Congestion(input_size=input_size, output_size=output_size)  # Initialize the model with the same architecture
 model.load_state_dict(torch.load(file_path_weights, map_location='cpu'))
 # Take beta results
@@ -376,12 +403,26 @@ model.eval()  # Set the model to evaluation mode
 with torch.no_grad():
     X_test = torch.tensor(X_test, dtype=torch.float32)
     # Squeeze the input tensor to match the Fc size
-    X_test = X_test.squeeze(dim=-1)
+    # X_test = X_test.squeeze(dim=-1)
     outputs = model(X_test)
-    outputs = outputs.squeeze(dim=-1)
-    beta = outputs.numpy()
-# Change Beta such will be in size L, N, K
-beta = - 1.0 * np.reshape(beta, (L, N, K))
+    # outputs = outputs.squeeze(dim=-1)
+    outputs = outputs.numpy()
+
+# Generate parameters to calculate beta
+a1 = outputs[:, 0:K]
+a2 = outputs[:, K:(2*K)]
+a1 = np.repeat(a1[:, np.newaxis, :], N, axis=1)
+a2 = np.repeat(a2[:, np.newaxis, :], N, axis=1)
+b1 = -1.0 * a1
+b2 = -1.0 * a2
+
+if Debug_flag:
+    # Debug
+    a1 = np.repeat(C[:, np.newaxis, :], N, axis=1)
+    a2 = np.repeat(B[:, np.newaxis, :], N, axis=1)
+    b1 = -1.0 * a1
+    b2 = -1.0 * a2
+
 
 # Main loop
 for t in range(T):
@@ -421,7 +462,12 @@ for t in range(T):
     # calculate gradient
     grad_nash = calc_gradient(C_k_nash_expanded, nash_Xn_k, B, C, Xe_nash, Nash_flag=True)
     grad_global = calc_gradient(C_k_global_expanded, global_Xn_k, B, C, Xe_global, Nash_flag=False)
+    ###############################################################################################################
+    Xe_nash_const_expand = np.repeat(Xe_nash_const[:, np.newaxis, :], N, axis=1)
+    beta = compute_beta(nash_const_Xn_k, Xe_nash_const_expand, a1=a1, a2=a2, b1=b1, b2=b2)
     grad_nash_const = calc_gradient(C_k_nash_const_expanded, nash_const_Xn_k, B, C, Xe_nash_const, Nash_flag=True, beta=beta, gamma=0) ### This NN
+    ###################################################################################################################################################
+
     # Calculate tax grad
     tax_regulizaer = B + 4 * C * Xe_nash_tax
     grad_nash_tax = C_k_nash_tax_expanded * (nash_tax_Xn_k > 0) + nash_tax_Xn_k * np.repeat((B + 2 * C * Xe_nash_tax)[:, np.newaxis, :], N, axis=1) \
@@ -452,6 +498,16 @@ if Generate_Train:
 
 # nash_const_record = DeBug_NN(B, C, Xe_global, global_Xn_k, nash_const_Xn_k, C_k_global)
 
+# Check Random solution
+if Random_flag:
+    random_Xn_k = Initialize_action(L, N, K)
+    Xe_random = np.sum(random_Xn_k, axis=1)
+    C_k_random = A + B * Xe_random + C * (Xe_random ** 2)
+    C_k_random_expand = np.repeat(C_k_random[:, np.newaxis, :], N, axis=1)
+    Cn_random = np.sum(random_Xn_k * C_k_random_expand, axis=2)
+    random_record = np.sum(Cn_random, axis=1)
+    random_record = np.repeat(random_record[np.newaxis, :], T, axis=0)
+
 # Plot results
 t = np.arange(T)
 plt.figure(1)
@@ -459,6 +515,7 @@ plt.plot(t, np.sum(global_record, axis=1), '--k', label="Global")
 plt.plot(t, np.sum(nash_record, axis=1), label="Nash")
 plt.plot(t, np.sum(nash_const_record, axis=1), label="NN")
 plt.plot(t, np.sum(nash_tax_record, axis=1), label="Tax")
+# plt.plot(t, np.sum(random_record, axis=1), label="Random")
 plt.xlabel("# Iterations"), plt.ylabel("# Score"), plt.legend()
 plt.show()
 
