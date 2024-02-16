392efc4e01fe3e933408e727f2600e8434972347
Ariel Kantorovich <ubuntu@ARIELKA-LAP.EMIT.local>
2024-02-16 17:27:17 +0200

    correct Xnash calc

--- Noise_Quadratic_SIM.py
+++ Noise_Quadratic_SIM.py
@@ -40,19 +40,16 @@ def generate_Q_B(N, L, alpha, beta, std_Q=0, mu_Q=0, subMean=False):
 
 def calculate_scores(Q, B, x_optimal, L):
     """
-    :param Q: size (L,N, N, N)
-    :param B: size (L, N, N, 1)
-    :param x_optimal: size (L, N, N, 1)
-    :param N_exp: scalar
-    :param N: scalar
+    :param Q: size (L,NxN)
+    :param B: size (L, Nx1)
+    :param x_optimal: size (L, Nx1)
     :param L: scalar
     :return: scores vector by size L
     """
     scores = np.zeros((L, ))
     for i in range(L):
         x = x_optimal[i]
-        x_transposed = np.transpose(x, (0, 2, 1))
-        scores[i] = np.sum(0.5 * x_transposed @ Q[i] @ x + x_transposed @ B[i])
+        scores[i] = np.sum(0.5 * np.transpose(x) @ Q[i] @ x + np.transpose(x) @ B[i])
     return scores
 
 def compute_gradient(Qn, Bn, x):
@@ -65,20 +62,42 @@ def compute_gradient(Qn, Bn, x):
     """
     return np.matmul(Qn, x) + Bn
 
+def compute_Nash_Vec(Qn, Bn):
+    """
+    :param Qn: size (L, N, NxN)
+    :param Bn: size (L, N, Nx1)
+    :return: x_nash size (L, N, 1)
+    """
+    L, N, _, _ = Qn.shape
+    x_nash = np.zeros((L, N, 1))
+    for l in range(L):
+        Q_new = np.zeros((N, N))
+        Q_diag = np.zeros((N, N))
+        B_new = np.zeros((N, 1))
+        for n in range(N):
+            Q_new[n, :] = Qn[l, n, n, :]
+            Q_diag[n, n] = Qn[l, n, n, n]
+            B_new[n] = Bn[l, n, n]
+        Q_psudo_inv = np.linalg.pinv(Q_new + Q_diag)
+        x_nash[l] = -2 * Q_psudo_inv @ B_new
+    return x_nash
+
+
 
 # %% Parameters
 L = 100 # samples of Q
 N = 5 # Number of players
 alpha = 4.0
-beta = 0.1
+beta = 1.0
 std_list = np.arange(0.1, 10.3, 0.1)
 mu = 0
 add_diag = False
-Border_projection = 70
+Border_projection = 2
 # Define final results arrays
 cost_optimal_list = np.zeros_like(std_list)
 cost_Q_noise_list = np.zeros_like(std_list)
 cost_X_noise_list = np.zeros_like(std_list)
+cost_X_fake_list = np.zeros_like(std_list)
 # Generate Q and Q_noise
 Q, B = generate_Q_B(N, L, alpha, beta, subMean=False)
 # Add diagonal from inverse Matrix
@@ -87,37 +106,43 @@ if add_diag:
     identity = c_diag * np.eye(N)
     Q = Q + identity[np.newaxis, :, :]
 # Repeat for N players
-B = np.repeat(B[:, np.newaxis, :, :], N, axis=1)
-Q = np.repeat(Q[:, np.newaxis, :, :], N, axis=1)
+B_repeat = np.repeat(B[:, np.newaxis, :, :], N, axis=1)
+Q_repeat = np.repeat(Q[:, np.newaxis, :, :], N, axis=1)
 # Inverse Matrices
 Q_inv = np.linalg.inv(Q)
 for i, std in enumerate(std_list):
     # Generate Q noise
-    Q_noise = Q + std * np.random.randn(L, N, N, N) + mu
-    # Inverse Matrices
-    Q_inv_noise = np.linalg.inv(Q_noise)
+    Q_noise = Q_repeat + std * np.random.randn(L, N, N, N) + mu
+    # Generate Q_fake for debug
+    temp = np.mean(Q_noise, axis=(2, 3))
+    Q_fake = temp[:, :, np.newaxis, np.newaxis] * np.ones((N, N))
     # Calculate X action of all three methods
     x_opt = np.matmul(-Q_inv, B)
-    x_QNoise = np.matmul(-Q_inv_noise, B)
-    x_Noise = x_opt + std * np.random.randn(L, N, N, 1) + mu
+    x_Noise = x_opt + std * np.random.randn(L, N, 1) + mu
+    x_QNoise = compute_Nash_Vec(Q_noise, B_repeat)  # calculate nash
+    x_fake = compute_Nash_Vec(Q_fake, B_repeat)
     # Project solution to solution space
     x_opt = np.minimum(np.maximum(x_opt, -Border_projection), Border_projection)
     x_QNoise = np.minimum(np.maximum(x_QNoise, -Border_projection), Border_projection)
     x_Noise = np.minimum(np.maximum(x_Noise, -Border_projection), Border_projection)
+    x_fake = np.minimum(np.maximum(x_fake, -Border_projection), Border_projection)
     # Calculate cost
     cost_optimal = calculate_scores(Q, B, x_opt, L)
     cost_Q_Noise = calculate_scores(Q, B, x_QNoise, L)
     cost_X_noise = calculate_scores(Q, B, x_Noise, L)
+    cost_X_fake = calculate_scores(Q, B, x_fake, L)
     # Sum on L games
     cost_optimal_list[i] = np.sum(cost_optimal)
     cost_Q_noise_list[i] = np.sum(cost_Q_Noise)
     cost_X_noise_list[i] = np.sum(cost_X_noise)
+    cost_X_fake_list[i] = np.sum(cost_X_fake)
 
 
 # Plot results
 plt.plot(std_list, cost_optimal_list, '--k', label='Opt')
 plt.plot(std_list, cost_Q_noise_list, label='Q Noise')
 plt.plot(std_list, cost_X_noise_list, label='X Noise')
+plt.plot(std_list, cost_X_fake_list, label='Q fake(Mean)')
 plt.legend(), plt.ylabel("# Cost"), plt.xlabel("$\sigma$"), plt.show()
 
 print("Finsh !!! !")
